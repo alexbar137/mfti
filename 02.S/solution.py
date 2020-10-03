cafab89988ba3b9b3d7a5a6ad27e4393660d1018
Александр Барабаш <alexbar137@gmail.com>
2020-10-03 17:19:03 +0700

    Task 02

---
+++ 02.S/solution.py
@@ -0,0 +1,17 @@
+import turtle as t
+
+def forwardRight():
+  for x in range(2):
+    t.forward(50)
+    t.left(90)
+
+def forwardLeft():
+  for x in range(2):
+    t.forward(50)
+    t.right(90)
+
+forwardRight()
+forwardLeft()
+t.forward(50)
+
+input()
