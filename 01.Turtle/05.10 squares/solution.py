66ba35c5f2ccd991c6f4469b4455506144adcfd5
Александр Барабаш <alexbar137@gmail.com>
2020-10-03 18:15:07 +0700

    Task 05

---
+++ 01.Turtle/05.10 squares/solution.py
@@ -0,0 +1,23 @@
+import turtle as t
+
+BASE_SIZE = 20
+
+def square(size):
+  for x in range(4):
+    t.forward(size)
+    t.right(90)
+
+def relocate():
+  DISTANCE = BASE_SIZE/2
+  t.penup()
+  t.back(DISTANCE)
+  t.left(90)
+  t.forward(DISTANCE)
+  t.right(90)
+  t.pendown()
+
+for x in range(1, 11):
+  square(x * BASE_SIZE)
+  relocate()
+
+input()
