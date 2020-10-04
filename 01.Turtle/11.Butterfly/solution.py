75ca9b9debce349a258f9a6931776ebf3c3f9445
Александр Барабаш <alexbar137@gmail.com>
2020-10-04 11:09:02 +0700

    Task 11

---
+++ 01.Turtle/11.Butterfly/solution.py
@@ -0,0 +1,19 @@
+import turtle as t
+import math
+
+BASE_DIAMETER = 20
+
+def drawCircle(diameter):
+  c = math.pi * abs(diameter)
+  direction = diameter/abs(diameter)
+  for x in range(36):
+    t.forward(c/10)
+    t.left(direction * 10)
+
+t.setheading(90)
+for x in range(10):
+  diameter = BASE_DIAMETER + x*5
+  drawCircle(diameter)
+  drawCircle(-diameter)
+
+t.done()
