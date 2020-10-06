f6866d99744fe557aae6a15ca2173ef2361685ff
Александр Барабаш <alexbar137@gmail.com>
2020-10-06 21:06:39 +0700

    Tasks 10 and 11

--- 02.Robot/robot-tasks-master/task_10.py
+++ 02.Robot/robot-tasks-master/task_10.py
@@ -5,7 +5,15 @@ from pyrob.api import *
 
 @task
 def task_8_3():
-    pass
+  while(not(wall_is_on_the_right())):
+    fill()
+    move_right()
+  else:
+    fill()
+
+def fill():
+  if (wall_is_above() or wall_is_beneath()):
+    fill_cell()
 
 
 if __name__ == '__main__':
--- 02.Robot/robot-tasks-master/task_11.py
+++ 02.Robot/robot-tasks-master/task_11.py
@@ -5,8 +5,15 @@ from pyrob.api import *
 
 @task
 def task_8_4():
-    pass
+  while(not(wall_is_on_the_right())):
+    fill()
+    move_right()
+  else:
+    fill()
 
+def fill():
+  if (wall_is_above() and wall_is_beneath()):
+    fill_cell()
 
 if __name__ == '__main__':
     run_tasks()
