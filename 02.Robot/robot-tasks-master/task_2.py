cc53a70de7914333b50c242d48eb4eead8974a91
Александр Барабаш <alexbar137@gmail.com>
2020-10-06 20:22:44 +0700

    Task 2.2

--- 02.Robot/robot-tasks-master/task_2.py
+++ 02.Robot/robot-tasks-master/task_2.py
@@ -5,7 +5,13 @@ from pyrob.api import *
 
 @task
 def task_1_2():
-  pass
+  for x in range(2):
+    move_down()
+  for x in range(4):
+    move_right()
+    if x == 1:
+      fill_cell()
+  move_down()
 
 
 if __name__ == '__main__':
